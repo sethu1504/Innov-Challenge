6bfd176156764cb9772f0d3b0967623121e9882c
sethuramanannamalai <sethu1504@gmail.com>
2020-01-19 15:17:10 -0800

    A dummy api

--- app/__init__.py
+++ app/__init__.py
@@ -3,3 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 from app import routes
+
+def create_app():
+    app = Flask(__name__)
+
+    # ...
+
+    from app.api import bp as api_bp
+    app.register_blueprint(api_bp, url_prefix='/api')
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,17 @@
 from app import app
+from flask import jsonify
+import pandas as pd
+import os
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+
+@app.route('/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+	SITE_ROOT = os.path.realpath(os.path.dirname('data/credit.csv'))
+	file_url = os.path.join(SITE_ROOT, 'credit.csv')
+	data = pd.read_csv(file_url)
+	return jsonify({'taskID': task_id})
