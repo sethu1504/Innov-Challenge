45a40523b9e11b2f8d0d6d44d0cb23c73a22395a
sethuramanannamalai <sethu1504@gmail.com>
2020-01-22 11:07:08 -0800

    Cluster stats and mapping points

--- app/utils.py
+++ app/utils.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import os
 import json
+import numpy as np
 from sklearn.cluster import KMeans
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.decomposition import PCA
 
 DATA_FOLDER = os.path.realpath(os.path.dirname('data/'))
 
@@ -44,3 +47,37 @@ def get_cluster_field_metadata(field_name):
     field_props['length'] = None
     field_props['multiValue'] = False
     return field_props
+
+
+def compute_cluster_stats(data, cluster_field_name, cluster_fields):
+    stats = dict()
+    clusters = data[cluster_field_name].unique()
+
+    for cluster in clusters:
+        cluster_data = data[data[cluster_field_name] == cluster]
+        cluster_data_stats = cluster_data.describe()
+        cluster_stat = dict()
+        cluster_stat['count'] = cluster_data.shape[0]
+        fields = dict()
+        for field in cluster_fields:
+            field_stats = dict()
+            print(cluster_data[field].dtype)
+            field_stats['mean'] = cluster_data_stats[field]['mean']
+            field_stats['min'] = cluster_data_stats[field]['min']
+            field_stats['max'] = cluster_data_stats[field]['max']
+            fields[field] = field_stats
+
+        cluster_stat['fields'] = fields
+        stats[cluster] = cluster_stat
+
+    return stats
+
+
+def get_mapping_points(data, cluster_fields):
+    cluster_data = data[cluster_fields]
+    distance = 1 - cosine_similarity(np.asarray(cluster_data))
+
+    pca = PCA(2)
+    pca.fit(distance)
+    pca_data = pca.transform(distance)
+    return pca_data
